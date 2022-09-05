c24f6be5dc74f125674c9e3ff4e02c8480b45bd4
권세훈(Kwon sehoon) <110405191+WorldHoon@users.noreply.github.com>
2022-09-05 13:17:09 +0900

    SpringBoot

---
+++ WorldHoon/src/main/java/com/example/WorldHoon/WorldHoonApplication.java
@@ -0,0 +1,13 @@
+package com.example.WorldHoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldHoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldHoonApplication.class, args);
+	}
+
+}
---
+++ WorldHoon/src/test/java/com/example/WorldHoon/WorldHoonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WorldHoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldHoonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
